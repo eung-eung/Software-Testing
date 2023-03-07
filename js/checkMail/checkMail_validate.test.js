47b2519b492bdb3e51491a22dae022fcab6b2e15
eung-eung <boyvip5231@gmail.com>
2023-03-07 07:42:42 +0700

    push newest code

--- js/checkMail/checkMail_validate.test.js
+++ js/checkMail/checkMail_validate.test.js
@@ -4,13 +4,13 @@ describe('Test mail', () => {
     test('case true format mail', () => {
         var stringName = 'a@gmail.com'
         expect(checkMail(stringName)).toBe(true)
-        expect(stringName).toMatch(/^a/)
+
     }
     )
     test('case false format mail', () => {
         var stringName = 'agmailcom'
         expect(checkMail(stringName)).toBe(false)
-        expect(stringName).toMatch(/^a/)
+
     }
     )
 
